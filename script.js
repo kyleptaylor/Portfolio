37fd36ebb2d82ae8794b2ad73942f819d388c31d
Kyle Taylor <kylepattontaylor@gmail.com>
2024-02-01 22:54:11 -0500

    Added name wave effect, and color effect on box hover using JS

--- script.js
+++ script.js
@@ -27,3 +27,121 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     }
   });
 });
+
+// Name Wave Effect //
+
+window.addEventListener('load', function () {
+    const nameSpanElements = [...document.querySelectorAll('h1.hover-text span')];
+    const titleSpanElements = [...document.querySelectorAll('h2.hover-text span')];
+
+    const delayBetweenSpans = 50;
+    
+    function applyWaveEffectToSpanElements(elements) {
+      elements.forEach(function (span, index) {
+          setTimeout(function () {
+              span.classList.add('hover-effect');
+              
+              setTimeout(function () {
+                  span.classList.remove('hover-effect');
+              }, 400);
+          }, index * delayBetweenSpans);
+      });
+      
+      // Now Reverse //
+
+      const initialAnimationDuration = elements.length * delayBetweenSpans + 400;
+
+      setTimeout(function () {
+        elements.slice().reverse().forEach(function (span, index) {
+          setTimeout(function () {
+              span.classList.add('hover-effect');
+              
+              setTimeout(function () {
+                  span.classList.remove('hover-effect');
+              }, 400);
+          }, index * delayBetweenSpans);
+        });
+      }, initialAnimationDuration);
+    }
+
+    applyWaveEffectToSpanElements(nameSpanElements);
+    applyWaveEffectToSpanElements(titleSpanElements);
+
+    // Run Function on Click //
+
+    const headerBox = document.querySelector('.header-box')
+    headerBox.addEventListener('click', () => { 
+      applyWaveEffectToSpanElements(nameSpanElements);
+      applyWaveEffectToSpanElements(titleSpanElements);
+    });
+});
+
+
+// End Name Wave Effect //
+
+// Hover On Sections Effect //
+
+document.addEventListener('DOMContentLoaded', function () {
+  const aboutTitle = document.getElementById('about')
+  const aboutBox = document.querySelector('.about-box');
+  const experienceTitle = document.getElementById('experience')
+  const experienceBox = document.querySelector('.experience-box');
+  const projectTitle = document.getElementById('projects')
+  const projectBox = document.querySelector('.project-box');
+  const customCursor = document.querySelector('.custom-cursor');
+
+  aboutBox.addEventListener('mouseenter', function (){
+    if (window.matchMedia("(min-width: 1000px)").matches) {
+      aboutTitle.style.transform = 'scale(1.03)';
+      aboutTitle.style.backgroundColor = '#385464';
+      aboutTitle.style.boxShadow = '#ffffff99 0px 0px 8px'
+    }
+  });
+
+  aboutBox.addEventListener('mouseleave', function (){
+    aboutTitle.style.transform = 'scale(1)';
+    aboutTitle.style.backgroundColor = '';
+    aboutTitle.style.boxShadow = ''
+  });
+
+  experienceBox.addEventListener('mouseenter', function () {
+      customCursor.style.border = '3px solid #1DCDFE';
+      customCursor.style.boxShadow = '0px 0px 150px 100px #1DCDFE50';
+      
+      if (window.matchMedia("(min-width: 1000px)").matches) {
+        experienceTitle.style.transform = 'scale(1.03)';
+        experienceTitle.style.backgroundColor = '#30526B';
+        experienceTitle.style.boxShadow = '#ffffff99 0px 0px 8px'
+      }
+  });
+
+  experienceBox.addEventListener('mouseleave', function () {
+      experienceTitle.style.transform = 'scale(1)';
+      customCursor.style.border = '';
+      customCursor.style.boxShadow = '';
+      experienceTitle.style.backgroundColor = '';
+      experienceTitle.style.boxShadow = ''
+  });
+
+  projectBox.addEventListener('mouseenter', function () {
+      customCursor.style.border = '3px solid #C19DFF';
+      customCursor.style.boxShadow = '0px 0px 150px 100px #C19DFF50';
+
+      if (window.matchMedia("(min-width: 1000px)").matches) {
+        projectTitle.style.transform = 'scale(1.03)';
+        projectTitle.style.backgroundColor = '#3D4D6B';
+        projectTitle.style.boxShadow = '#ffffff99 0px 0px 8px'
+      }
+  });
+
+  projectBox.addEventListener('mouseleave', function () {
+      projectTitle.style.transform = 'scale(1)';
+      customCursor.style.border = '';
+      customCursor.style.boxShadow = '';
+      projectTitle.style.backgroundColor = '';
+      experienceTitle.style.boxShadow = ''
+  });
+
+});
+
+// End Hover On Sections Effect //
